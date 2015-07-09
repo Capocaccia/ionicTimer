7543215fb3e823932e25eb2460753e72730cfe3e
Simone Gonzalez <simone.b.gonzalez@gmail.com>
2015-07-09 15:02:54 -0500

    interval countdown and styling

--- www/js/mainCtrl.js
+++ www/js/mainCtrl.js
@@ -1,5 +1,5 @@
 angular.module('starter', [])
-.controller('mainCtrl', function($scope, $interval){
+.controller('mainCtrl', function($scope, $interval, $timeout){
   $scope.input = "";
   $scope.display = $scope.input;
   //$scope.seconds = 50;
@@ -10,6 +10,7 @@ angular.module('starter', [])
   }
 
   $scope.play = function() {
+      if($scope.input >= 1) {
       $interval(function() {
       $scope.input*1;
       $scope.input-=1;
@@ -18,7 +19,10 @@ angular.module('starter', [])
       console.log($scope.display);
 
     }, 1000);
-
+    }
+    else if($scope.input = 0) {
+      $interval.cancel(stop);
+    }
   };
 
   $scope.pause = function() {
