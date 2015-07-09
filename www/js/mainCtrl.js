d8ea66f14f934c919882a1f4c39d7e2518ac02cc
Simone Gonzalez <simone.b.gonzalez@gmail.com>
2015-07-09 14:38:56 -0500

    yay it works!

--- www/js/mainCtrl.js
+++ www/js/mainCtrl.js
@@ -1,5 +1,27 @@
 angular.module('starter', [])
-.controller('mainCtrl', function($scope){
+.controller('mainCtrl', function($scope, $interval){
+  $scope.input = "";
+  $scope.display = $scope.input;
+  //$scope.seconds = 50;
 
+  $scope.update = function(number) {
+    $scope.input += number;
+    $scope.display = $scope.input;
+  }
 
+  $scope.play = function() {
+      $interval(function() {
+      $scope.input*1;
+      $scope.input-=1;
+      $scope.display = $scope.input;
+
+      console.log($scope.display);
+
+    }, 1000);
+
+  };
+
+  $scope.pause = function() {
+    window.clearInterval();
+  }
 });
